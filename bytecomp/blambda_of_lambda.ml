047cb65dc48f5f84c120db6da08ea2c03951b71d
Joseph Cutler <jwc@seas.upenn.edu>
2025-08-01 13:39:34 -0400

    its the identity

--- bytecomp/blambda_of_lambda.ml
+++ bytecomp/blambda_of_lambda.ml
@@ -750,8 +750,7 @@ let rec comp_expr (exp : Lambda.lambda) : Blambda.blambda =
       pseudo_event (variadic (Makeblock { tag = Config.lazy_tag }))
     | Pmakelazyblock Forward_tag ->
       pseudo_event (variadic (Makeblock { tag = Obj.forward_tag }))
-    | Preinterpret_word_as_value ->
-      Misc.fatal_errorf "Blambda_of_lambda: %a is not supported in bytecode"
-        Printlambda.primitive primitive)
+    | Preinterpret_word_as_value -> (
+      match args with [arg] -> comp_arg arg | _ -> wrong_arity ~expected:1))
 
 let blambda_of_lambda x = comp_expr x
