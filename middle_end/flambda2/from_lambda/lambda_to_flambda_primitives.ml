832c2cb6e25bd44d6452cbf9793acb262328e538
Joseph Cutler <jwc@seas.upenn.edu>
2026-01-25 15:16:36 -0500

    String and bigstring indexing through naked nativeint instead of naked immediate

--- middle_end/flambda2/from_lambda/lambda_to_flambda_primitives.ml
+++ middle_end/flambda2/from_lambda/lambda_to_flambda_primitives.ml
@@ -677,10 +677,16 @@ let convert_index_to_tagged_int ~index ~(index_kind : Lambda.array_index_kind) =
              },
            index ))
 
+let convert_index_to_naked_nativeint ~index ~(index_kind : Lambda.array_index_kind)
+    =
+  let src = convert_lambda_index_to_standard_int_or_float index_kind in
+  H.Prim (Unary (Num_conv { src; dst = Naked_nativeint }, index))
+
 let convert_index_to_untagged_int ~index ~(index_kind : Lambda.array_index_kind)
     =
   let src = convert_lambda_index_to_standard_int_or_float index_kind in
   H.Prim (Unary (Num_conv { src; dst = Naked_immediate }, index))
+
 
 let convert_index_to_naked_int64 ~index ~(index_kind : Lambda.array_index_kind)
     =
@@ -893,7 +899,7 @@ let string_like_load ~dbg ~unsafe
     (kind : P.string_like_value) mode ~boxed string ~index_kind index
     ~current_region =
   let unsafe_load =
-    let index = convert_index_to_untagged_int ~index ~index_kind in
+    let index = convert_index_to_naked_nativeint ~index ~index_kind in
     let wrap =
       match access_size, mode with
       | (Eight | Sixteen), None ->
